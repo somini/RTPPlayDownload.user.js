a0aa35bd0d00d353d587fd5102c2425cc97f910f
somini <somini29@yandex.com>
2015-01-29 21:33:54 +0000

    Initial commit

---
+++ RTP_Download.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        RTP Play Download
+// @description Inclui links directos nas páginas do RTP Play. Não funciona para alguns vídeos.
+// @description Quando não funciona mostra as flashvars
+// @namespace   somini
+// @include	http://www.rtp.pt/play/p*/e*
+// @include	http://www.rtp.pt/programa/tv/p*/c*
+// @grant       none
+// @version     1.3
+// ==/UserScript==
